74cda0b147c0fc61179b31521b1cc48c85ecd129
Jane Seah <seah.jane@gmail.com>
2024-06-30 23:13:39 +0800

    Switch crypto from expo to web crypto api

--- src/api/core.ts
+++ src/api/core.ts
@@ -1,14 +1,17 @@
 import { generateClient } from "aws-amplify/api";
-import * as Crypto from "expo-crypto";
-import { Buffer } from "buffer";
+
+export const graphqlClient = generateClient();
 
 const BASE62_CHARSET =
   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-export const graphqlClient = generateClient();
-
-export function hashToBase62(hash: Uint8Array, length: number): string {
-  let bigint = BigInt("0x" + Buffer.from(hash).toString("hex"));
+function hashToBase62(hash: Uint8Array): string {
+  let bigint = BigInt(
+    "0x" +
+      Array.from(hash)
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join(""),
+  );
 
   let base62 = "";
   while (bigint > 0) {
@@ -16,20 +19,18 @@ export function hashToBase62(hash: Uint8Array, length: number): string {
     bigint /= 62n;
   }
 
-  // Ensure the base-62 string is the given length
-  return base62.padStart(length, "0").substring(0, length);
+  // Ensure the base-62 string is exactly 8 characters long
+  return base62.padStart(8, "0").substring(0, 8);
 }
 
 export async function generateCustomerSpecificShortId(
   customerId: string,
   uuid: string,
-  length: number
 ): Promise<string> {
   const combinedString = customerId + uuid;
-  const hash = await Crypto.digestStringAsync(
-    Crypto.CryptoDigestAlgorithm.SHA256,
-    combinedString,
-    { encoding: Crypto.CryptoEncoding.HEX }
-  );
-  return hashToBase62(Buffer.from(hash, "hex"), length);
+  const encoder = new TextEncoder();
+  const data = encoder.encode(combinedString);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = new Uint8Array(hashBuffer);
+  return hashToBase62(hashArray);
 }
