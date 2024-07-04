8568e744dead87d4db7b79efa275e9cb1b00482f
delarta-tok <delarta.tok@gmail.com>
2024-07-05 05:52:26 +0700

    fix update booking status

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -22,6 +22,7 @@ import {
   Modal,
   Col,
   Row,
+  message,
 } from "antd";
 import PawkitLogo from "../../assets/pawkit_logo.png";
 import { fetchAuthSession, signOut } from "aws-amplify/auth";
@@ -39,7 +40,7 @@ function getItem(
   label: React.ReactNode,
   key: React.Key,
   icon?: React.ReactNode,
-  children?: MenuItem[],
+  children?: MenuItem[]
 ): MenuItem {
   return {
     key,
@@ -61,10 +62,14 @@ const Dashboard: React.FC = () => {
   } = theme.useToken();
   const [bookingList, setBookingList] = useState<any[]>();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const [openedRecord, setOpenedRecord] = useState<any>({});
   const [bookingStatus, setBookingStatus] = useState<BookingStatus | null>(
-    null,
+    null
   );
+
+  const [messageApi, contextHolder] = message.useMessage();
+
   const navigate = useNavigate();
 
   const columns: TableProps<ListBookingsQuery>["columns"] = useMemo(
@@ -153,7 +158,7 @@ const Dashboard: React.FC = () => {
       },
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [bookingList, openedRecord],
+    [bookingList, openedRecord]
   );
 
   const handleCancel = () => {
@@ -168,6 +173,20 @@ const Dashboard: React.FC = () => {
     setBookingStatus(record.status);
   };
 
+  const getBookings = async () => {
+    setLoading(true);
+    try {
+      const result = await fetchBookings({});
+      if (result) {
+        setBookingList(result);
+      }
+    } catch (err: any) {
+      message.error(`error when fetching data: ${err?.message}`, 2.5);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     (async () => {
       const authSession = (
@@ -178,45 +197,38 @@ const Dashboard: React.FC = () => {
       }
     })();
 
-    const getBookings = async () => {
-      try {
-        const result = await fetchBookings({});
-        if (result) {
-          setBookingList(result);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
     getBookings();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleUpdateBooking = async (
     customerUsername: string,
-    timeSlotId: string,
+    timeSlotId: string
   ) => {
-    console.log(customerUsername, timeSlotId, bookingStatus);
     try {
+      messageApi.open({
+        type: "loading",
+        content: "Action in progress..",
+        duration: 2.5,
+      });
       if (bookingStatus) {
         const result = await updateBookingStatus(
           customerUsername,
           timeSlotId,
           bookingStatus,
-          false,
+          false
         );
         if (result) {
+          getBookings();
+
+          message.success("Update Booking Success", 2.5);
           setIsModalOpen(false);
           setOpenedRecord({});
           setBookingStatus(null);
         }
       }
-      // if (result) {
-      //   setBookingList(result);
-      // }
-    } catch (err) {
-      console.log(err);
+    } catch (err: any) {
+      message.error(`Update Booking failed: ${err?.message}`, 2.5);
     }
   };
 
@@ -224,8 +236,8 @@ const Dashboard: React.FC = () => {
     try {
       await signOut();
       navigate("/login");
-    } catch (error) {
-      console.log(error);
+    } catch (err: any) {
+      message.error(`Error sign out: ${err?.message}`, 2.5);
     }
   };
 
@@ -234,188 +246,202 @@ const Dashboard: React.FC = () => {
   };
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider
-        theme="light"
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-      >
-        <div
-          style={{
-            padding: 20,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <img width={100} src={PawkitLogo} />
-        </div>
-        <Menu
+    <>
+      {contextHolder}
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
           theme="light"
-          defaultSelectedKeys={["1"]}
-          mode="inline"
-          items={items}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Flex
-            gap="middle"
-            justify="flex-end"
-            align="center"
-            style={{ height: "100%", padding: 24 }}
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div
+            style={{
+              padding: 20,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <Avatar icon={<UserOutlined />} />
-            <Typography>Admin</Typography>
+            <img width={100} src={PawkitLogo} />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["1"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Flex
+              gap="middle"
+              justify="flex-end"
+              align="center"
+              style={{ height: "100%", padding: 24 }}
+            >
+              <Avatar icon={<UserOutlined />} />
+              <Typography>Admin</Typography>
 
-            <Button type="text" danger onClick={handleSignOut}>
-              Sign Out
-            </Button>
-          </Flex>
-        </Header>
-        <Content style={{ margin: "0 16px" }}>
-          <div
-            style={{
-              margin: "16px 0",
-              padding: 16,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            <Flex gap="middle">
-              <RangePicker />
-              <Select
-                // defaultValue="all"
-                placeholder="Service Type"
-                style={{ width: 180 }}
-                // onChange={handleChange}
-                options={[
-                  { value: "all", label: "All" },
-                  { value: "grooming", label: "Grooming" },
-                  { value: "vaccination", label: "Vaccination" },
-                ]}
-              />
-              <Select
-                // defaultValue="all"
-                placeholder="Status"
-                style={{ width: 180 }}
-                // onChange={handleChange}
-                options={[
-                  { value: "all", label: "All" },
-                  { value: "in progress", label: "In Progress" },
-                  { value: "cancelled", label: "Cancelled" },
-                  { value: "confirmed", label: "Confirmed" },
-                ]}
-              />
+              <Button type="text" danger onClick={handleSignOut}>
+                Sign Out
+              </Button>
             </Flex>
-          </div>
-          <div
-            style={{
-              margin: "16px 0",
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            <Table columns={columns} dataSource={bookingList} />
-          </div>
-          <Modal
-            title="Booking Detail"
-            open={isModalOpen}
-            // footer={null}
-            onOk={() =>
-              handleUpdateBooking(
-                openedRecord?.customerUsername,
-                openedRecord?.timeSlotId,
-              )
-            }
-            okText="Update Booking Status"
-            onCancel={handleCancel}
-          >
-            <Typography.Title level={3}>
-              ID: {openedRecord?.id}
-            </Typography.Title>
-            <Flex vertical gap={24} style={{ marginBottom: 30 }}>
-              <Row>
-                <Col span={12}>
-                  <Typography.Text type="secondary">
-                    Date Created
-                  </Typography.Text>
-                  <br />
-                  <Typography.Text>
-                    {openedRecord?.startDateTime
-                      ? format(
-                          new Date(openedRecord?.startDateTime),
-                          "yyyy/MM/dd hh:mm aa",
-                        )
-                      : "-"}
-                  </Typography.Text>
-                </Col>
-                <Col span={12}>
-                  <Typography.Text type="secondary">Booking ID</Typography.Text>
-                  <br />
-                  <Typography.Text>
-                    {openedRecord?.id ? openedRecord?.id : "-"}
-                  </Typography.Text>
-                </Col>
-              </Row>
-              <Row>
-                <Col span={12}>
-                  <Typography.Text type="secondary">Owner Name</Typography.Text>
-                  <br />
-                  <Typography.Text>
-                    {openedRecord?.customerUsername
-                      ? openedRecord?.customerUsername
-                      : "-"}
-                  </Typography.Text>
-                </Col>
-                <Col span={12}>
-                  <Typography.Text type="secondary">Status</Typography.Text>
-                  <br />
-                  {/* <Typography.Text>
+          </Header>
+          <Content style={{ margin: "0 16px" }}>
+            {/* 
+               <div
+              style={{
+                margin: "16px 0",
+                padding: 16,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <Flex gap="middle">
+                <RangePicker />
+                <Select
+                  // defaultValue="all"
+                  placeholder="Service Type"
+                  style={{ width: 180 }}
+                  // onChange={handleChange}
+                  options={[
+                    { value: "all", label: "All" },
+                    { value: "grooming", label: "Grooming" },
+                    { value: "vaccination", label: "Vaccination" },
+                  ]}
+                />
+                <Select
+                  // defaultValue="all"
+                  placeholder="Status"
+                  style={{ width: 180 }}
+                  // onChange={handleChange}
+                  options={[
+                    { value: "all", label: "All" },
+                    { value: "in progress", label: "In Progress" },
+                    { value: "cancelled", label: "Cancelled" },
+                    { value: "confirmed", label: "Confirmed" },
+                  ]}
+                />
+              </Flex>
+            </div>
+           */}
+
+            <div
+              style={{
+                margin: "16px 0",
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <Table
+                columns={columns}
+                dataSource={bookingList}
+                loading={loading}
+              />
+            </div>
+            <Modal
+              title="Booking Detail"
+              open={isModalOpen}
+              // footer={null}
+              onOk={() =>
+                handleUpdateBooking(
+                  openedRecord?.customerUsername,
+                  openedRecord?.timeSlotId
+                )
+              }
+              okText="Update Booking Status"
+              onCancel={handleCancel}
+            >
+              <Typography.Title level={3}>
+                ID: {openedRecord?.id}
+              </Typography.Title>
+              <Flex vertical gap={24} style={{ marginBottom: 30 }}>
+                <Row>
+                  <Col span={12}>
+                    <Typography.Text type="secondary">
+                      Date Created
+                    </Typography.Text>
+                    <br />
+                    <Typography.Text>
+                      {openedRecord?.startDateTime
+                        ? format(
+                            new Date(openedRecord?.startDateTime),
+                            "yyyy/MM/dd hh:mm aa"
+                          )
+                        : "-"}
+                    </Typography.Text>
+                  </Col>
+                  <Col span={12}>
+                    <Typography.Text type="secondary">
+                      Booking ID
+                    </Typography.Text>
+                    <br />
+                    <Typography.Text>
+                      {openedRecord?.id ? openedRecord?.id : "-"}
+                    </Typography.Text>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col span={12}>
+                    <Typography.Text type="secondary">
+                      Owner Name
+                    </Typography.Text>
+                    <br />
+                    <Typography.Text>
+                      {openedRecord?.customerUsername
+                        ? openedRecord?.customerUsername
+                        : "-"}
+                    </Typography.Text>
+                  </Col>
+                  <Col span={12}>
+                    <Typography.Text type="secondary">Status</Typography.Text>
+                    <br />
+                    {/* <Typography.Text>
                     {openedRecord?.status ? openedRecord?.status : "-"}
                   </Typography.Text> */}
 
-                  <Select
-                    // defaultValue="all"
-                    value={bookingStatus}
-                    placeholder="Status"
-                    style={{ width: 180 }}
-                    onChange={handleChangeStatus}
-                    options={[
-                      {
-                        value: BookingStatus.COMPLETED,
-                        label: BookingStatus.COMPLETED,
-                      },
-                      {
-                        value: BookingStatus.CONFIRMED,
-                        label: BookingStatus.CONFIRMED,
-                      },
-                      {
-                        value: BookingStatus.PENDING,
-                        label: BookingStatus.PENDING,
-                      },
-                      {
-                        value: BookingStatus.IN_PROGRESS,
-                        label: BookingStatus.IN_PROGRESS,
-                      },
-                      {
-                        value: BookingStatus.CANCELLED,
-                        label: BookingStatus.CANCELLED,
-                      },
-                    ]}
-                  />
-                </Col>
-              </Row>
-            </Flex>
-          </Modal>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Pawkit ©{new Date().getFullYear()} Created by Pawkit Dev
-        </Footer>
+                    <Select
+                      // defaultValue="all"
+                      value={bookingStatus}
+                      placeholder="Status"
+                      style={{ width: 180 }}
+                      onChange={handleChangeStatus}
+                      options={[
+                        {
+                          value: BookingStatus.COMPLETED,
+                          label: BookingStatus.COMPLETED,
+                        },
+                        {
+                          value: BookingStatus.CONFIRMED,
+                          label: BookingStatus.CONFIRMED,
+                        },
+                        {
+                          value: BookingStatus.PENDING,
+                          label: BookingStatus.PENDING,
+                        },
+                        {
+                          value: BookingStatus.IN_PROGRESS,
+                          label: BookingStatus.IN_PROGRESS,
+                        },
+                        {
+                          value: BookingStatus.CANCELLED,
+                          label: BookingStatus.CANCELLED,
+                        },
+                      ]}
+                    />
+                  </Col>
+                </Row>
+              </Flex>
+            </Modal>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Pawkit ©{new Date().getFullYear()} Created by Pawkit Dev
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   );
 };
 
